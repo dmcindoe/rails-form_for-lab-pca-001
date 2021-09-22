b0aa8f5765748f04aabfc6599ab31b9a7aceb097
Andrew McIndoe <amcindoe@powerhrg.com>
2021-09-22 11:12:52 -0400

    Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Jon", last_name: "Bow")
+SchoolClass.create(title: "Coding Life 201", room_number: 42)
+Student.create(first_name: "Rob", last_name: "Borish")
+SchoolClass.create(title: "Cooking Stuff 101", room_number: 12)
+Student.create(first_name: "Ike", last_name: "Michaels")
+SchoolClass.create(title: "Hitchhiking 104", room_number: 7)
